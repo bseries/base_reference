b2256d21baf40d39cb426a21b8ba2b18f0dea3eb
Phillip Klein <phillip@atelierdisko.de>
2018-01-10 15:07:45 +0100

    Costumize CC short forms and urls.

--- models/Licenses.php
+++ models/Licenses.php
@@ -33,13 +33,44 @@ class Licenses extends \base_core\models\Base {
 			if (!$result) {
 				return false;
 			}
+
+			$url = $result[2];
+
+			if (strpos($name, 'CC') === 0) {
+				// Preserve CC short forms according to
+				// https://creativecommons.org/licenses/
+				//
+				// CC-BY-SA-3.0 -> CC BY-SA 3.0
+				// CC0-1.0 -> CC0 1.0
+				$pos = strpos($name, '-');
+				if ($pos !== false) {
+					$name = substr_replace($name, ' ', $pos, 1);
+				}
+				$rpos = strrpos($name, '-');
+				if ($rpos !== false) {
+					$name = substr_replace($name, ' ', $rpos, 1);
+				}
+
+				// Use custom urls for CC references
+				$ccUrl = function ($name) {
+					$baseUrl = 'https://creativecommons.org/';
+					if (strpos($name, 'CC0') === 0) {
+						return $baseUrl . 'about/cc0/';
+					}
+					$type = strtolower(substr($name, 3, -4));
+					$version = substr($name, -3);
+					return $baseUrl . 'licenses/' . $type . '/' . $version . '/';
+				};
+
+				$url = $ccUrl($name);
+			}
 			// Remap to our data structure, pretend as if this is a database
 			// result. We might later turn this into a real entity object.
 			return static::create([
 				'name' => $name,
 				'title' => $result[0],
 				'is_osi_certified' => $result[1],
-				'url' => $result[2]
+				'url' => $url
 			]);
 		});
 	}
